4896b1486557ad7167fb0f5658dad14575cf5e78
itproto <itprotoltd@gmail..com>
2024-07-29 22:38:36 +0100

    Added shared ui-package

---
+++ packages/itpr-client-ui/src/components/fancy-button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface FancyButtonProps {
+    label: string;
+}
+
+export const FancyButton: React.FC<FancyButtonProps> = ({ label }) => {
+    return (
+        <button className="fancy-button">
+            {label}
+        </button>
+    );
+};
